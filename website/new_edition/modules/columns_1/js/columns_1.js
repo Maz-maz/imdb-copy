d3b1064fc55b333d3d57cab1c7d472e61fab98dc
Revlos <revlos@protonmail.com>
2017-08-30 20:14:18 +0300

    Improved columns_1 module. Added classes for changing the width relations between the columns.

--- website/new_edition/modules/columns_1/js/columns_1.js
+++ website/new_edition/modules/columns_1/js/columns_1.js
@@ -1,38 +1,44 @@
 var columns_1 = document.querySelectorAll(".columns_1");
 
-/*Check if this javascript already applied once*/
+
+var columns_1_lenght_class_list = ["columns_1__2", "columns_1__3", "columns_1__4", "columns_1__5", "columns_1__6"];
+
+function classToNum(x) {
+  for (j = 0; j < columns_1_lenght_class_list.length; j++) {
+    if (x.includes(columns_1_lenght_class_list[j]) === true) {
+      return j+2;
+    };
+  };
+  return 1;
+};
 
 
-for (i=0; i < columns_1.length; i++) {
+for(i = 0; i < columns_1.length; i++) {
+  var columns_1_w = columns_1[i].getBoundingClientRect().width;
 
-  var columns_1_class = columns_1[i].getAttribute("class");
+  var columns_1__column = columns_1[i].querySelectorAll(".columns_1__column");
+  var columns_1__divider = columns_1[i].querySelectorAll(".columns_1__divider");
 
-  if (columns_1_class.includes("columns_1_done") === false ) {
+  var columns_1__divider_w = 0;
 
-    var columns_1__column = columns_1[i].querySelectorAll(".columns_1__column");
-    var columns_1__divider = columns_1[i].querySelectorAll(".columns_1__divider");
+  if (columns_1__divider.length !== 0) {
+    columns_1__divider_w = columns_1__divider[0].getBoundingClientRect().width;
+  };
 
-    if (columns_1__divider.length !== 0) {
+  if (columns_1__column.length !== 0) {
 
-      var columns_1_width = columns_1[i].getBoundingClientRect().width;
-      var columns_1__column_number = columns_1__column.length;
-      var columns_1__divider_number = columns_1__divider.length;
-      var columns_1__divider_width = columns_1__divider[0].getBoundingClientRect().width;
-      var columns_1__column_width = (columns_1_width - columns_1__divider_number*columns_1__divider_width)/columns_1__column_number;
+    var columns_1__divider_num = columns_1__divider.length;
+    var columns_1__column_num = 0;
+  
+    for (k = 0; k < columns_1__column.length; k++) {
+      columns_1__column_num += classToNum(columns_1__column[k].getAttribute("class"));
+    };
 
-      for (j=0; j < columns_1__column.length; j++) {
-        columns_1__column[j].setAttribute("style","width: " + columns_1__column_width+"px");
-      };
-    } else {
-      var columns_1_width = columns_1[i].getBoundingClientRect().width;
-      var columns_1__column_number = columns_1__column.length;
-      var columns_1__column_width = columns_1_width/columns_1__column_number;
+    var columns_1__column_w = (columns_1_w - columns_1__divider_num*columns_1__divider_w)/columns_1__column_num;
 
-      for (j=0; j < columns_1__column.length; j++) {
-        columns_1__column[j].setAttribute("style","width: " + columns_1__column_width+"px");
-      };
-    }
+    for (a = 0; a < columns_1__column.length; a++) {
+      columns_1__column[a].setAttribute("style", "width: " + columns_1__column_w*classToNum(columns_1__column[a].getAttribute("class")) + "px");
+    };
 
-    columns_1[i].setAttribute("class", columns_1_class + " columns_1_done");
   };
 };
