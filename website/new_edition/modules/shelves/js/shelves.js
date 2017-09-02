900bb7386dc449cbecf921b08c26b44bc7b624d1
Revlos <revlos@protonmail.com>
2017-09-02 15:56:07 +0300

    Begening of applaying responsovness for the site. Particularly made responsivesome responsivness of the main and aside, true adding slider.

---
+++ website/new_edition/modules/shelves/js/shelves.js
@@ -0,0 +1,53 @@
+var shelves__css_applayed = document.querySelector(".shelves__css_applayed");
+var shelves__responsive_slide = document.querySelector(".shelves__responsive_slide");
+var main = document.querySelector("main");
+var aside = document.querySelector("aside");
+
+/*Only if applied shelves-layout_600.css
+shelves__css_applayed.getBoundingClientRect().width === 1*/
+/*!!! Can be done better ?*/
+
+function shelves__responsive_slide_MvRight () {
+  shelves__responsive_slide.setAttribute("style", "right: 0;");
+};
+
+function shelves__responsive_slide_MvLeft () {
+  shelves__responsive_slide.setAttribute("style", "right: 50%;");
+};
+
+/*!!! Add a hint for the user for this actions*/
+main.addEventListener("click", function() {
+  if(shelves__css_applayed.getBoundingClientRect().width === 1) {
+    shelves__responsive_slide_MvRight();
+  };
+});
+
+aside.addEventListener("click", function() {
+  if(shelves__css_applayed.getBoundingClientRect().width === 1) {
+    shelves__responsive_slide_MvLeft();
+  };
+});
+
+/*
+var setTime_1;
+var setTime_2;
+
+main.addEventListener("mouseover", function() {
+  if(shelves__css_applayed.getBoundingClientRect().width === 1) {
+    setTime_1 = setTimeout(shelves__responsive_slide_MvRight , 3000);
+  }
+});
+main.addEventListener("mouseleve", function() {
+  clearTimeout(setTime_1);
+});
+
+
+aside.addEventListener("mouseover", function() {
+  if(shelves__css_applayed.getBoundingClientRect().width === 1) {
+    setTime_2 = setTimeout(shelves__responsive_slide_MvLeft , 3000);
+  }
+});
+aside.addEventListener("mouseleve", function() {
+  clearTimeout(setTime_2);
+});
+*/
